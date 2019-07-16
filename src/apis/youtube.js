625e6cd99d9e4351149bf61b024072a891c06721
Aamir Khan <47424913+aamirkhancr7@users.noreply.github.com>
2019-07-17 02:08:22 +0530

    Update youtube.js

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -3,10 +3,5 @@ import axios from 'axios';
 const KEY = 'AIzaSyBgCTY8pFd2wYQsrM__kBs6hGRxE5hpdtA';
 
 export default axios.create({
-    baseURL: 'https://www.googleapis.com/youtube/v3',
-    params: {
-        part: 'snippet',
-        maxResults: 50,
-        key: KEY
-    }
+    baseURL: 'https://www.googleapis.com/youtube/v3'
 });
